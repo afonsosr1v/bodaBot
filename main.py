acc7283ac4da4b5195e1de3aee54f5e4bdd981ad
Afonso Nóbrega <afonsosriv@gmail.com>
2023-10-04 17:35:21 +0100

    modified:   main.py

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 import nextcord
 import config 
 import os
-import requests
-from bs4 import BeautifulSoup
 from nextcord.ext import commands
 from nextcord import Interaction, SlashOption, ChannelType, User
 from nextcord.abc import GuildChannel
+from googleapiclient.discovery import build
 
 intents = nextcord.Intents.all()
 intents.members = True
-server_id = 688100174433878081
+
 #gis = GoogleImagesSearch(config.GOOGLESEARCHAPI, config.CX)
 
 bot = commands.Bot(command_prefix='$', intents=intents)
@@ -25,11 +24,11 @@ async def on_ready():
 #        bot.load_extension(f'cogs.{filename[:-3]}')
 
 
-@bot.slash_command(name="ping", description="Pinga de Volta para Teste", guild_ids=[server_id])
+@bot.slash_command(name="ping", description="Pinga de Volta para Teste", guild_ids=[config.SERVERID])
 async def ping(interaction: nextcord.Interaction):
     await interaction.response.send_message("Ping")
 
-@bot.slash_command(guild_ids=[server_id], name="review", description="Comando para Review de Albuns")
+@bot.slash_command(guild_ids=[config.SERVERID], name="review", description="Comando para Review de Albuns")
 async def echo(
     interaction: nextcord.Interaction,
     artista: str,
@@ -42,16 +41,12 @@ async def echo(
         description= f"Opinião: {opinião}",
         color = nextcord.Color.red()
     )
-    ''' #Pesquisa da capa do album, n estpi a conseguir fazer funcionar
-    query = f"{artista} {album} album cover"
-    pesquisa = f"https://www.google.com/search?q={query}&tbm=isch"
-    resposta = requests.get(pesquisa)
-    soup = BeautifulSoup(resposta.text, "lxml")
-    img_tag = soup.find("img", {"class": "yWs4tf"})
-    img_link = img_tag.get("src")
+    #Pesquisa da capa do album, n estpi a conseguir fazer funcionar
+    resource = build("customsearch", "v1", developerKey=config.GOOGLESEARCHAPI).cse()
+    result = resource.list(q=f"{artista} {album}", cx=config.CX, searchType="image").execute()
+    img_link = result['items'][0]['link']
     
-    
-    embed.set_thumbnail(url=img_link) #Dar set da capa do album '''
+    embed.set_image(url=img_link) #Dar set da capa do album '''
     embed.add_field(name="Review", value=f"{nota}/10")
     embed.add_field(name="Reviewer", value=interaction.user)
     await interaction.response.send_message(embed=embed)
