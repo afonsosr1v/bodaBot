d16041ced4524c85832e80313912f174e20fb0f6
RicardoGuerraFCT <rfr.guerra@campus.fct.unl.pt>
2023-10-11 17:42:48 +0100

    0.3.0

--- config.py
+++
@@ -1,4 +0,0 @@
-TOKEN = "NULL" #Bot Token
-GOOGLESEARCHAPI = "" #Google Search API Key
-CX = "NULL" #Google Search CX Key
-SERVERID = 0 #Discord Server ID
--- review.py
+++ review.py
@@ -1,24 +1,36 @@
 # review.py
 
 from rng import generate_random_string
-from flask import abort
+from flask import abort,  make_response
 
 REVIEW = {
-    generate_random_string(): {
+    "1": {
+        "reviewID": "1",
+        "reviewerID": "1",
+        "serverID": "1",
         "artista": "a",
         "album": "b",
+        "albumImage": "ya",
         "score": "c",
         "review": "d",
     },
-    generate_random_string(): {
+    "2": {
+        "reviewID": "2",
+        "reviewerID": "1",
+        "serverID": "1",
         "artista": "A",
         "album": "B",
+        "albumImage": "ya",
         "score": "C",
         "review": "D",
     },
-    generate_random_string(): {
+    "3": {
+        "reviewID": "3",
+        "reviewerID": "1",
+        "serverID": "1",
         "artista": "AA",
         "album": "BB",
+        "albumImage": "ya",
         "score": "CC",
         "review": "DD",
     }
@@ -28,22 +40,49 @@ def read_all():
     return list(REVIEW.values())
 
 def create(review):
+    reviewID = generate_random_string()
+    reviewerID = "1"
+    serverID = "1"
     artista = review.get("artista")
     album = review.get("album")
+    albumImage = "ya"
     score = review.get("score")
-    review = review.get("review", "")
-    Id = generate_random_string()
+    review = review.get("review")
 
-    if artista and album not in REVIEW:
-        REVIEW[Id] = {
+    if reviewerID and artista and album not in REVIEW:
+        REVIEW[reviewID] = {
+            "reviewID": reviewID,
+            "reviewerID": reviewerID,
+            "serverID": serverID,
             "artista": artista,
             "album": album,
+            "albumImage": albumImage,
             "score": score,
             "review": review,
         }
-        return REVIEW[Id], 201
+        return REVIEW[reviewID], 201
     else:
         abort(
             406,
-            f"Album {album} already exists",
+            f"Review for {album} already exists",
+        )
+
+def read_reviewID(reviewID):
+    if reviewID in REVIEW:
+        return REVIEW[reviewID]
+    else:
+        abort(
+            404, f"Review with ID {reviewID} not found"
+        )
+
+def delete(reviewID):
+    if reviewID in REVIEW:
+        del REVIEW[reviewID]
+        return make_response(
+            f"{reviewID} apagado com sucesso", 200
+        )
+    else:
+        abort(
+            404,
+            f"Review {reviewID} não encontrada"
         )
