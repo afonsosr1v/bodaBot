88319186a76847c7ca0a2bba92130dbb0f5ecad9
RicardoGuerraFCT <rfr.guerra@campus.fct.unl.pt>
2023-10-14 12:11:26 +0100

    0.3.2

--- website.py
+++ website.py
@@ -1,5 +1,5 @@
 from flask import render_template
-from flask_bootstrap import Bootstrap5
+#from flask_bootstrap import Bootstrap5
 
 import config
 from models import User
@@ -8,12 +8,12 @@ from models import User
 app = config.connex_app
 app.add_api(config.basedir / "swagger.yml")
 
-bootstrap = Bootstrap5(app)
+#bootstrap = Bootstrap5(app)
 
 @app.route("/")
 def home():
     users = User.query.all()
-    return render_template("home.html", users=users)
+    return render_template("index.html", users=users)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
